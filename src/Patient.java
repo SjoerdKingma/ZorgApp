d75d42459e4fdd850f3511f38bc8abf56d96c823
SjoerdKingma <Sjoerdk@outlook.com>
2022-09-08 16:11:32 +0200

    Initial commit

---
+++ src/Patient.java
@@ -0,0 +1,59 @@
+import java.time.LocalDate;
+import java.time.Period;
+
+public class Patient
+{
+   private static final int RETURN      = 0;
+   private static final int SURNAME     = 1;
+   private static final int FIRSTNAME   = 2;
+   private static final int DATEOFBIRTH = 3;
+
+   private int       id;
+   private String    surname;
+   private String    firstName;
+   private LocalDate dateOfBirth;
+
+   ////////////////////////////////////////////////////////////////////////////////
+   ////////////////////////////////////////////////////////////////////////////////
+   public String getSurname()
+   {
+      return surname;
+   }
+
+   ////////////////////////////////////////////////////////////////////////////////
+   ////////////////////////////////////////////////////////////////////////////////
+   public String getFirstName()
+   {
+      return firstName;
+   }
+
+   ////////////////////////////////////////////////////////////////////////////////
+   // Constructor
+   ////////////////////////////////////////////////////////////////////////////////
+   Patient( int id, String surname, String firstName, LocalDate dateOfBirth )
+   {
+      this.id          = id;
+      this.surname     = surname;
+      this.firstName   = firstName;
+      this.dateOfBirth = dateOfBirth;
+   }
+
+   ////////////////////////////////////////////////////////////////////////////////
+   // Display patient data.
+   ////////////////////////////////////////////////////////////////////////////////
+   public void viewData()
+   {
+      System.out.format( "===== Patient id=%d ==============================\n", id );
+      System.out.format( "%-17s %s\n", "Surname:", surname );
+      System.out.format( "%-17s %s\n", "firstName:", firstName );
+      System.out.format( "%-17s %s\n", "Date of birth:", dateOfBirth );
+   }
+
+   ////////////////////////////////////////////////////////////////////////////////
+   // Shorthand for a Patient's full name
+   ////////////////////////////////////////////////////////////////////////////////
+   public String fullName()
+   {
+      return String.format( "%s %s [%s]", firstName, surname, dateOfBirth.toString() );
+   }
+}
---
+++ src/ZorgApp.java
@@ -0,0 +1,10 @@
+public class ZorgApp
+{
+   public static void main( String[] args )
+   {
+      User           user           = new User( 1, "Mart ElCamera" );
+      Administration administration = new Administration( user );
+
+      administration.menu();
+   }
+}

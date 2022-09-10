6f7f70bfe1b12d1c6616614a90a73f7f43ead270
SjoerdKingma <Sjoerdk@outlook.com>
2022-09-10 13:20:32 +0200

    Adding filewriter to save the current user.

--- src/Administration.java
+++ src/Administration.java
@@ -13,12 +13,19 @@ public class Administration
 {
    private static final int STOP = 0;
    private static final int VIEW = 1;
+   private static final int CHANGE_USER = 2;
 
    private Patient currentPatient;            // The currently selected patient
    private User    currentUser;               // the current user of the program.
+
+   private ArrayList<User> userList;
+   private ArrayList<Patient> patientList;
+
    Administration( ArrayList<User> users )
    {
-      currentUser    = users.get(0);
+      userList = users;
+      int currentUserId = SettingsEditor.GetCurrentUser();
+      currentUser    = getUserFromUserId(currentUserId);
       currentPatient = new Patient(
               1, "Van Puffelen", "Pierre",
               LocalDate.of( 2000, 2, 29 ),
@@ -38,6 +45,7 @@ public class Administration
          // Print menu on screen
          System.out.format( "%d:  STOP\n", STOP );
          System.out.format( "%d:  View patient data\n", VIEW );
+         System.out.format( "%d:  Change User\n", CHANGE_USER );
 
          System.out.print( "enter #choice: " );
          int choice = scanner.nextInt();
@@ -50,11 +58,25 @@ public class Administration
             case VIEW:
                currentUser.viewPatientData( currentPatient );
                break;
+            case CHANGE_USER:{
+               System.out.println("Enter UserId: ");
+               int userId = scanner.nextInt();
+               changeUser(userId);
+               break;
+            }
 
             default:
                System.out.println( "Please enter a *valid* digit" );
                break;
          }
       }
+   }
+
+   void changeUser(int userId){
+      SettingsEditor.UpdateCurrentUser(userId);
+   }
+
+   User getUserFromUserId(int userId){
+      return userList.stream().filter(x->x.getUserID() == userId).findFirst().get();
    }
 }
---
+++ src/SettingsEditor.java
@@ -0,0 +1,72 @@
+import java.io.*;
+
+public final class SettingsEditor {
+
+    private final static String SettingsPath = "CurrentUser.txt";
+
+    private SettingsEditor(){
+
+    }
+
+    public static int GetCurrentUser(){
+
+        try{
+            BufferedReader reader = new BufferedReader(new FileReader(SettingsPath));
+            int userId = 0;
+            try{
+                userId = Integer.parseInt(reader.readLine());
+            }
+            catch(NumberFormatException ex){
+                ex.printStackTrace();
+            }
+            reader.close();
+            return userId;
+
+        }catch(IOException e){
+            e.printStackTrace();
+            return 0;
+        }
+    }
+
+    public static void UpdateCurrentUser(int userId) {
+        try{
+            BufferedWriter writer = new BufferedWriter(new FileWriter(SettingsPath));
+            writer.write(userId);
+            writer.close();
+            System.out.println("UserId succesfully changed!");
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+}
+    /*public static void ChangeUser(int userId){
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            public void run(){
+                //save data here
+                int CurrentUserId = userId;
+                System.out.println("Saving userId: "+ userId);
+            }
+        });
+    }*/
+
+    /*public static void ChangeUserJson(int userId){
+        String settingsPath = "Settings.json";
+
+        String path = "/app/json/companies.json";
+
+        JSONObject json = new JSONObject();
+        try {
+            json.put("name", "Google");
+            json.put("employees", 140000);
+            json.put("offices", List.of("Mountain View", "Los Angeles", "New York"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        try (PrintWriter out = new PrintWriter(new FileWriter(path))) {
+            out.write(json.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }*/
+

51ddaf8169c3b279dc6e78c30ac9e4f187e1c976
SjoerdKingma <Sjoerdk@outlook.com>
2022-10-19 08:28:28 +0200

    Fixed the MenuEditPatient voor dentist because it was showing fields which should be hidden from the dentist.

--- src/Tandarts.java
+++ src/Tandarts.java
@@ -69,8 +69,6 @@ public class Tandarts extends User{
         System.out.format("%d: %-17s %s\n", SURNAME, "Surname:", patient.getSurname());
         System.out.format("%d: %-17s %s\n", FIRST_NAME, "FirstName:", patient.getFirstName());
         System.out.format("%d: %-17s %s\n", DATE_OF_BIRTH, "Date of birth:", patient.getDateOfBirth());
-        System.out.format("%d: %-17s %s\n", WEIGHT, "Weight: ", patient.getWeight());
-        System.out.format("%d: %-17s %s\n", LENGTH, "Length: ", ConversionHelper.addTwoDecimals(patient.getLength()));
         System.out.format("%d: %-17s \n", MEDICINES, "Medicine list");
     }
 }

3af007c7484003b265d3b55a92e06e4946c7ddbb
SjoerdKingma <Sjoerdk@outlook.com>
2022-10-21 11:40:19 +0200

    Changed outlining when displaying all patients.

--- src/PatientManager.java
+++ src/PatientManager.java
@@ -18,7 +18,7 @@ public class PatientManager {
 
         for(int i=0; i<patients.size(); i++){
             Patient p = patients.get(i);
-            System.out.format("%-3d %-20s %-16s Patiënt ID: %s\n", i, p.getSurname(), p.getDateOfBirth(), p.getPatientId());
+            System.out.format("%d. %-20s %-16s Patiënt ID: %s\n", i, p.getSurname(), p.getDateOfBirth(), p.getPatientId());
         }
     }
 
